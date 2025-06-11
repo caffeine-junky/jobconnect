5371224102176fe77051a92b16b31d446529d622
Moses Kubeka <mkay.py@gmail.com>
2025-06-12 01:41:26 +0200

    Initialized frontend repository

---
+++ frontend/src/App.tsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+	return (
+		<div className="w-screen h-screen flex items-center justify-center">
+			<h1 className="text-3xl font-bold">JobConnect</h1>
+		</div>
+	);
+}
